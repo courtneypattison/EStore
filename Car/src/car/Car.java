d3d63c759c123d3c6ebed22519d50becc536aced
courtneypattison <courtney@courtneypattison.com>
2016-10-21 11:17:56 -0400

    added functions for each menu option already created

--- Car/src/car/Car.java
+++ Car/src/car/Car.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * @author Courtney Bodi
  */
 public class Car {
-    
+
     enum MenuOption {
         NULL,
         ENTER_VEHICLE,
@@ -19,17 +19,21 @@ public class Car {
         FILE_DATA_DUMP,
         EXIT
     }
-    
+
     public static final String NEW_LINE = System.lineSeparator();
+
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Car> cars = new ArrayList<>();
 
     private int year;
     private String brandAndModel;
     private int price;
-   
+
     /**
      * Car constructor
+     *
      * @param year
-     * @param brandAndModel 
+     * @param brandAndModel
      * @param price
      */
     public Car(int year, String brandAndModel, int price) {
@@ -37,9 +41,10 @@ public class Car {
         this.brandAndModel = brandAndModel;
         this.price = price;
     }
-    
+
     /**
      * Car constructor with default price
+     *
      * @param year
      * @param brandAndModel
      */
@@ -48,7 +53,7 @@ public class Car {
         this.brandAndModel = brandAndModel;
         price = 50000;
     }
-    
+
     /**
      * Default Car constructor
      */
@@ -57,21 +62,9 @@ public class Car {
         brandAndModel = "";
         price = 0;
     }
-    
-    public void dataDump() {
-        System.out.println(brandAndModel + " " + year + " " + price);
-    }
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        MenuOption userChoice = MenuOption.ENTER_VEHICLE;
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Car> cars = new ArrayList<>();
-        
-        do {
-            System.out.println("(1) Enter the info about a new vehicle (either a car or a SUV)." + NEW_LINE
+
+    private void printMenu() {
+        System.out.println("(1) Enter the info about a new vehicle (either a car or a SUV)." + NEW_LINE
                 + "(2) Print out brand and model, delimited by spaces, for all vehicles." + NEW_LINE
                 + "(3) Print average vehicle cost, as well as total number of vehicles." + NEW_LINE
                 + "(4) Print out all vehicle years." + NEW_LINE
@@ -79,81 +72,108 @@ public class Car {
                 + "(6) Standard Data dump." + NEW_LINE
                 + "(7) File Data dump." + NEW_LINE
                 + "(8) End program");
-            
+    }
+
+    private void promptUserAddVehicle() {
+        int year, price;
+        String brandAndModel;
+        Car newCar;
+
+        System.out.println("Enter vehicle brand and model:");
+        brandAndModel = scanner.nextLine();
+        String tokensCheck[] = brandAndModel.split(" +");
+        if (tokensCheck.length != 2 || tokensCheck[0].matches("")) {
+            System.out.println("Invalid input: You must enter a brand and model.");
+            return;
+        }
+
+        System.out.println("Enter year:");
+        String yearString = scanner.nextLine();
+        if (yearString.matches("^\\d+$")) {
+            year = Integer.parseInt(yearString);
+        } else {
+            System.out.println("Invalid input: You must enter a number.");
+            return;
+        }
+
+        System.out.println("Enter price, or leave blank:");
+        String priceString = scanner.nextLine();
+        System.out.println("price: " + priceString);
+        if (priceString.matches("^\\d+$")) {
+            price = Integer.parseInt(priceString);
+            newCar = new Car(year, brandAndModel, price);
+        } else if (priceString.matches("")) {
+            newCar = new Car(year, brandAndModel);
+        } else {
+            System.out.println("Invalid input: You must enter a number.");
+            return;
+        }
+
+        cars.add(newCar);
+    }
+
+    private void printBrandAndModel() {
+        for (Car car : cars) {
+            String tokens[] = car.brandAndModel.split(" +");
+            System.out.println("Brand: " + tokens[0] + NEW_LINE
+                    + "Model: " + tokens[1] + NEW_LINE);
+        }
+    }
+
+    private void printAvgAndTotal() {
+        double sum = 0;
+        double average = 0;
+
+        for (Car car : cars) {
+            sum += car.price;
+        }
+
+        if (cars.size() > 0) {
+            average = sum / (double) cars.size();
+        }
+
+        System.out.println("Average vehicle cost: " + average + NEW_LINE
+                + "Total number of vehicles: " + cars.size());
+    }
+
+    private void printYears() {
+        System.out.println("Vehicle years:");
+        for (Car car : cars) {
+            System.out.println(car.year);
+        }
+    }
+
+    private void dataDump() {
+        System.out.println(brandAndModel + " " + year + " " + price);
+    }
+
+    public void executeCommandLoop() {
+        MenuOption userChoice = MenuOption.ENTER_VEHICLE;
+
+        do {
+            printMenu();
+
             String userString = scanner.nextLine();
-            
+
             if (userString.matches("[12345678]")) {
                 userChoice = MenuOption.values()[Integer.parseInt(userString)];
             } else {
                 System.out.println("You must enter a digit between 1 and 8.");
                 continue;
             }
-            
+
             switch (userChoice) {
                 case ENTER_VEHICLE:
-                    int year, price;
-                    String brandAndModel;
-                    Car newCar;
-                    
-                    System.out.println("Enter vehicle brand and model:");
-                    brandAndModel = scanner.nextLine();
-                    String tokensCheck[] = brandAndModel.split(" +");
-                    if (tokensCheck.length != 2 || tokensCheck[0].matches("")) {
-                        System.out.println("Invalid input: You must enter a brand and model.");
-                        continue;
-                    }
-                    
-                    System.out.println("Enter year:");
-                    String yearString = scanner.nextLine();
-                    if (yearString.matches("^\\d+$")) {
-                        year = Integer.parseInt(yearString);
-                    } else {
-                        System.out.println("Invalid input: You must enter a number.");
-                        continue;
-                    }
-                    
-                    System.out.println("Enter price, or leave blank:");
-                    String priceString = scanner.nextLine();
-                    System.out.println("price: " + priceString);
-                    if (priceString.matches("^\\d+$")) {
-                        price = Integer.parseInt(priceString);
-                        newCar = new Car(year, brandAndModel, price);
-                    } else if (priceString.matches("")) {
-                        newCar = new Car(year, brandAndModel);
-                    } else {
-                        System.out.println("Invalid input: You must enter a number.");
-                        continue;
-                    }
-                    
-                    cars.add(newCar);
+                    promptUserAddVehicle();
                     break;
                 case PRINT_BRAND_AND_MODEL:
-                    for (Car car : cars) {
-                        String tokens[] = car.brandAndModel.split(" +");
-                        System.out.println("Brand: " + tokens[0] + NEW_LINE
-                            + "Model: " + tokens[1] + NEW_LINE);
-                    }
+                    printBrandAndModel();
                     break;
                 case PRINT_AVG_AND_TOTAL:
-                    double sum = 0;
-                    double average = 0;
-                    
-                    for (Car car : cars) {
-                        sum += car.price;
-                    }
-                    
-                    if (cars.size() > 0) {
-                        average = sum / (double)cars.size();
-                    }
-                    
-                    System.out.println("Average vehicle cost: " + average + NEW_LINE
-                        + "Total number of vehicles: " + cars.size());
+                    printAvgAndTotal();
                     break;
                 case PRINT_YEARS:
-                    System.out.println("Vehicle years:");
-                    for (Car car : cars) {
-                        System.out.println(car.year);
-                    }
+                    printYears();
                     break;
                 case READ_INPUT:
                     break;
@@ -168,5 +188,15 @@ public class Car {
                     break;
             }
         } while (userChoice != MenuOption.EXIT);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.executeCommandLoop();
+
     }
 }
